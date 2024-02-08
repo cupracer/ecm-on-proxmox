a22ceb8321c5338f664a24cc216609e98eb0083e
Thomas Schulte <thomas@cupracer.de>
2024-02-08 13:15:13 +0100

    add output objects

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,11 @@ output "nodes" {
   value = module.nodes
 }
 
+output "control_plane_nodes" {
+  value = { for i, n in module.nodes : i => n if n.node_type == "control_plane" }
+}
+
+output "worker_nodes" {
+  value = { for i, n in module.nodes : i => n if n.node_type == "worker" }
+}
+
