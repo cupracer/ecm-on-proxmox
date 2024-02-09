73fb86769b71afbcfef11c56841719a9daeb07c7
Thomas Schulte <thomas.schulte@suse.com>
2024-02-09 13:44:50 +0100

    format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
 module "proxies" {
   depends_on = [ module.nodes ]
+  source     = "./modules/reverse_proxy"
 
-  source = "./modules/reverse_proxy"
-
-  ssh_private_key = local.root_private_key
-  nodes = local.proxy_nodes
+  ssh_private_key     = local.root_private_key
+  nodes               = local.proxy_nodes
   control_planes_fqdn = [for entry in local.control_planes_map : entry.fqdn]
 }
 
@@ -13,15 +12,15 @@ module "k3s" {
   count      = var.k3s_version != null ? 1 : 0
   source     = "./modules/k3s"
 
-  ssh_private_key = local.root_private_key
-  proxy_nodes = local.proxy_nodes
-  cluster_name = local.cluster_name
+  ssh_private_key     = local.root_private_key
+  proxy_nodes         = local.proxy_nodes
+  cluster_name        = local.cluster_name
   control_plane_nodes = local.control_plane_nodes
-  worker_nodes = local.worker_nodes
-  set_taints = true # TODO: MAKE CONFIGURABLE
+  worker_nodes        = local.worker_nodes
+  set_taints          = true # TODO: MAKE CONFIGURABLE
   primary_master_fqdn = local.primary_master_fqdn
   primary_master_host = local.primary_master_host
-  k3s_version = var.k3s_version
+  k3s_version         = var.k3s_version
 }
 
 provider "helm" {
@@ -35,9 +34,9 @@ module "kured" {
   count      = var.kured_version != null ? 1 : 0
   source     = "./modules/kured"
 
-  ssh_private_key = local.root_private_key
+  ssh_private_key     = local.root_private_key
   control_plane_nodes = local.control_plane_nodes
-  worker_nodes = local.worker_nodes
-  kured_version = var.kured_version
+  worker_nodes        = local.worker_nodes
+  kured_version       = var.kured_version
 }
 
