b7cc3025acbca87efd81562a2f576cebce878035
Thomas Schulte <thomas@cupracer.de>
2024-03-29 15:00:02 +0100

    add system-upgrade-controller

--- terraform/modules/system_upgrade_controller/main.tf
+++ terraform/modules/system_upgrade_controller/main.tf
@@ -10,14 +10,7 @@ resource "ssh_resource" "setup_system_upgrade_controller" {
 
   commands = [
     "curl -L -o /var/lib/rancher/k3s/server/manifests/system-upgrade-controller.yaml https://github.com/rancher/system-upgrade-controller/releases/download/${var.system_upgrade_controller_version}/system-upgrade-controller.yaml",
+    "curl -L -o /var/lib/rancher/k3s/server/manifests/system-upgrade-controller-crd.yaml https://github.com/rancher/system-upgrade-controller/releases/download/${var.system_upgrade_controller_version}/crd.yaml",
   ]
-
-#  file {
-#    destination = "/var/lib/rancher/k3s/server/manifests/system-upgrade-controller.yaml"
-#    owner = "root"
-#    group = "root"
-#    permissions = "0644"
-#    source = "https://github.com/rancher/system-upgrade-controller/releases/download/${var.system_upgrade_controller_version}/system-upgrade-controller.yaml"
-#  }
 }
 
