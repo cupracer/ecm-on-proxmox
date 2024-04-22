57af3a18d7a014dcff825b8d3c0fa234d690c27e
Thomas Schulte <thomas@cupracer.de>
2024-04-22 11:32:41 +0200

    add rke_config for downstream

--- terraform/modules/rancher_downstream/main.tf
+++ terraform/modules/rancher_downstream/main.tf
@@ -1,5 +1,13 @@
 resource "rancher2_cluster_v2" "create_downstream_cluster" {
   name = var.downstream_cluster_name
   kubernetes_version = var.k3s_version
+
+  rke_config {
+    machine_global_config= <<EOF
+disable:
+  - servicelb
+  - local-storage
+EOF
+  }
 }
 
