fa12f796d3864fa00fd4b100e33be0e3cc9a694e
Thomas Schulte <thomas.schulte@suse.com>
2024-11-08 10:38:50 +0100

    move commands from packer to vm_post_install

--- terraform/modules/vm_post_install/main.tf
+++ terraform/modules/vm_post_install/main.tf
@@ -7,10 +7,20 @@ resource "ssh_resource" "grow_filesystem" {
   user         = "root"
   private_key  = var.ssh_private_key
 
-  commands = [
+  pre_commands = [
     "btrfs fi resize max /var",
-#    "transactional-update --no-selfupdate pkg install -y cri-tools kubernetes-client llvm clang",
-#    "systemctl stop sshd.service && reboot"
+    "ln -fs /usr/share/zoneinfo/Europe/Berlin /etc/localtime",
+  ]
+
+  commands = [<<-EOT
+    transactional-update --no-selfupdate pkg install --recommends -y \
+      cri-tools kubernetes-client llvm clang \
+      iptables bind-utils mtr tcpdump wireguard-tools \
+      restorecond setools-console \
+      open-iscsi nfs-client cifs-utils \
+      bash-completion git cryptsetup \
+      command-not-found policycoreutils-python-utils
+    systemctl stop sshd.service && reboot
+  EOT
   ]
 }
-
